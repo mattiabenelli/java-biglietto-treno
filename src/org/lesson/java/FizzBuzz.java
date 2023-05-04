50db008319c890b43a5b81cfbeeb935e471d7833
mattiabenelli <benellimattia0@gmail.com>
2023-05-04 17:14:08 +0200

    aggiunto numero intervallo da input

--- src/org/lesson/java/FizzBuzz.java
+++ src/org/lesson/java/FizzBuzz.java
@@ -1,14 +1,23 @@
 package org.lesson.java;
 
+import java.util.Scanner;
+
 public class FizzBuzz {
 	
 	public static void main(String[] args) {
-		for(int i = 1; i < 101; i++) {
+		
+		Scanner in = new Scanner (System.in);
+		int MaxNumber;
+		
+		System.out.println("Inserisci il numero massimo dell'elenco: ");
+		MaxNumber = in.nextInt();
+		System.out.println(" -------------------------- ");
+		
+		for(int i = 1; i <= MaxNumber; i++) {
 			
 			if(i %3 == 0 && i%5 != 0) {
 				System.out.println("Fizz");
 			}
-			
 			else if(i %3 != 0 && i %5 == 0) {
 				System.out.println("Buzz");
 			}

b24cf0b7775b20bef7aeede7fe90bdc754302307
mattiabenelli <benellimattia0@gmail.com>
2023-05-04 15:43:55 +0200

    bonus esercizio bonus

--- src/org/lesson/java/CibiPreferiti.java
+++ src/org/lesson/java/CibiPreferiti.java
@@ -14,9 +14,26 @@ public class CibiPreferiti {
 		PrimoClassifica = CibiPreferiti[0];
 		UltimoClassifica = CibiPreferiti[lngClassfica - 1];
 		
+		
 		System.out.println("La tua classifica è lunga: " + lngClassfica);
 		System.out.println("Il tuo preferito è: " + PrimoClassifica);
 		System.out.println("Il tuo meno preferito è: " + UltimoClassifica);
+		
+		if(CibiPreferiti.length %2 == 0) {
+			String x = CibiPreferiti[(CibiPreferiti.length/2) - 1];
+//	        System.out.println(x);
+	        String y = CibiPreferiti[CibiPreferiti.length/2];
+//	        System.out.println(y);
+	        
+	        System.out.println("Gli elementi di mezzo sono: " + x + "," + y);
+		}
+		else {
+			String z = CibiPreferiti[CibiPreferiti.length/2];
+	        System.out.println(z);
+	        
+	        System.out.println("L'elemento di mezzo è: " + z);
+		}
+		
 		
 	}
 }
